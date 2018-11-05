b967353f70c83b5fa1f399ee1912c10657ef6966
liangxianwen <444926345@qq.com>
2018-11-05 17:23:24 +0800

    Initial commit

---
+++ helloDemo/src/com/nlpdemo/NlpUtil.java
@@ -0,0 +1,65 @@
+package com.nlpdemo;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
+import edu.stanford.nlp.ling.CoreAnnotations.NamedEntityTagAnnotation;
+import edu.stanford.nlp.ling.CoreAnnotations.PartOfSpeechAnnotation;
+import edu.stanford.nlp.ling.CoreAnnotations.SentencesAnnotation;
+import edu.stanford.nlp.ling.CoreAnnotations.TextAnnotation;
+import edu.stanford.nlp.ling.CoreAnnotations.TokensAnnotation;
+import edu.stanford.nlp.ling.CoreLabel;
+import edu.stanford.nlp.pipeline.Annotation;
+import edu.stanford.nlp.pipeline.StanfordCoreNLP;
+import edu.stanford.nlp.util.CoreMap;
+
+public class NlpUtil {
+    public static void main(String[] args) {
+        // creates a StanfordCoreNLP object, with POS tagging, lemmatization,
+        // NER, parsing, and coreference resolution
+        Properties props = new Properties();
+        props.setProperty("annotators", "tokenize, ssplit, pos, lemma, ner, parse, dcoref");
+        StanfordCoreNLP pipeline = new StanfordCoreNLP(props);
+
+        // read some text in the text variable
+        String text = "She went to America last week.";// Add your text here!
+        // create an empty Annotation just with the given text
+        Annotation document = new Annotation(text);
+
+        // run all Annotators on this text
+        pipeline.annotate(document);
+
+
+
+        // these are all the sentences in this document
+        // a CoreMap is essentially a Map that uses class objects as keys and
+        // has values with custom types
+        List<CoreMap> sentences = document.get(SentencesAnnotation.class);
+
+        List<String> words = new ArrayList<>();
+        List<String> posTags = new ArrayList<>();
+        List<String> nerTags = new ArrayList<>();
+        for (CoreMap sentence : sentences) {
+            // traversing the words in the current sentence
+            // a CoreLabel is a CoreMap with additional token-specific methods
+            for (CoreLabel token : sentence.get(TokensAnnotation.class)) {
+                // this is the text of the token
+                String word = token.get(TextAnnotation.class);
+                words.add(word);
+                // this is the POS tag of the token
+                String pos = token.get(PartOfSpeechAnnotation.class);
+                posTags.add(pos);
+                // this is the NER label of the token
+                String ne = token.get(NamedEntityTagAnnotation.class);
+                nerTags.add(ne);
+            }
+        }
+
+        System.out.println(words.toString());
+        System.out.println(posTags.toString());
+        System.out.println(nerTags.toString());
+
+    }
+
+}
